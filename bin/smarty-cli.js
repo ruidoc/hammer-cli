454d7a287d59bda065d229ca304acf6c6ad5761a
yangchenggong <2218466341@qq.com>
2018-07-21 15:14:14 +0800

    first commit

--- bin/smarty-cli.js
+++ bin/smarty-cli.js
@@ -7,7 +7,19 @@ const log = require('tracer').colorConsole()
 const ora = require('ora')
 
 program
-    .version('1.0.0')
-    .option('-i, init [name]', '初始化smarty项目')
-
+    .version('0.1.0')
+    .command('init <project>')
+    .description('初始化一个smarty模板')
+    .action((project)=> {
+        if(!project) {
+            log.error('正确命令：smarty init <project>')
+        } else {
+            let pwd = shell.pwd()
+            log.info(`下载模板到：${pwd}/${project}/ ...`)
+            clone(`https://github.com/ruidoc/smarty`, `${pwd}/${project}`, null, ()=> {
+                shell.rm('-rf', `${pwd}/${project}/.git`)
+                log.info('初始化完成!')
+            })
+        }
+    })
 program.parse(process.argv)
