e5201b66a94a225976fd99d85f8f9f84e8a9ad94
Ruidoc <ruidocgo@gmail.com>
2021-08-05 22:40:43 +0800

    修改格式

--- bin/hammer-cli.js
+++ bin/hammer-cli.js
@@ -10,24 +10,24 @@ const Generator = require('../lib/generater')
 const log = console.log
 
 function resolve(dir) {
-    return path.join(shell.pwd().toString(), dir)
+  return path.join(shell.pwd().toString(), dir)
 }
 
 program
-  .version(`${require('../package').version}`,'-v --version')
-    .description(chalk.yellow('welcome to use hammer！'))
+  .version(`${require('../package').version}`, '-v --version')
+  .description(chalk.yellow('welcome to use hammer！'))
 
 program.command('gene <file>')
-    .description('create a new project')
-    .action(file => { 
-        var json = require(resolve(file))
-        let res = new Generator(json, {
-            output: path.dirname(resolve(file))
-        })
-        if (res) {
-            log(chalk.green('vue 组件创建成功！'))
-        }
+  .description('create a new project')
+  .action(file => {
+    var json = require(resolve(file))
+    let res = new Generator(json, {
+      output: path.dirname(resolve(file))
     })
-  
+    if (res) {
+      log(chalk.green('组件创建成功！'))
+    }
+  })
+
 // 解析参数并触发commands
 program.parse(process.argv);
---
+++ example/Table.jsx
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from 'react';
+import { useLocalStore, observer } from 'mobx-react';
+
+const IndexPage = observer(props => {
+var a = 1;var b = 3;
+})
+
+export default IndexPage
